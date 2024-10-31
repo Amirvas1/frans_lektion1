0fbca2143a86ba2b3188f42cc993f63f8005c456
Amir Vasconcelos <amir.vasconcelos@outlook.com>
2024-10-31 16:21:30 +0100

    Ditt commit-meddelande

---
+++ lektion.py
@@ -0,0 +1,112 @@
+# a = 10
+# b = 20
+# print (a + b)
+# words = "I like apples"
+# print(words.replace("apples", "bananas"))
+# words = "Hello World"
+# print(words.find("World")
+# empty_list = {}
+
+# my_list = ["apple, 42, true, 3.14"]
+
+# print(my_list[0])
+# numbers = [1,2,3,4,5]
+# numbers.append(6)
+# print(numbers)
+
+# numbers = [1,2,3,4,5]
+
+# numbers.insert(2, "hej")
+# print(numbers)
+
+# fruits = ["apple", "banana", "melon", "cherry"]
+# fruits.remove("melon")
+# print(fruits)
+
+# fruits = ["apple", "banana", "melon", "cherry, pear"]
+
+# print(fruits[:3])
+
+# numbers = [2,7,4,5,1]
+# print(sum(numbers))
+
+# empty_dict = {}
+
+# person = {"name": "Bobbo", "age": 37, "city": "Linköping"}
+
+# print(person["name"])
+
+# person = {"name": "Bobbo", "age": 37, "city": "Linköping"}
+
+# print(person.get("proffession", "Unknown"))
+# person["age"] = 26
+# print(person)
+
+# person ["proffession"] = "Hacker"
+# print(person)
+
+# age = person.pop("age")
+# print("age")
+# print("person")
+
+# del person ["name"]
+# print(person)
+
+# person["proffession"] = "Hacker"
+# print(person)
+
+# key, value = person.popitem()
+# print(key, value)
+# print(person)
+
+# empty_tuple = ()
+
+# small_tuple = (5,)
+
+# fruits = "apple", "melon", "cherry"
+# print(fruits)
+# print(type(fruits))
+
+# fruits = ("apple", "melon", "cherry")
+# print(fruits[0])
+# print(fruits[-1])
+
+# fruits = ("apple", "melon", "cherry")
+# fruits_list = list(fruits)
+# fruits_list[1] = "banana"
+# fruits = tuple(fruits_list)
+# print(fruits)
+
+# empty_set = set()
+
+# fruits = {"apple", "banana", "cherry", "apple"}
+
+# print(fruits)
+
+# fruits.add("grape")
+# print(fruits)
+
+# fruits.update(["orange", "mango"])
+# fruits.remove("banana")
+# print(fruits)
+
+# set1 = {1,2,3}
+# set2 = {3,4,5}
+
+# union_set = set1.union(set2)
+# print(union_set)
+
+# intersection_set = set1 & set2
+# print(intersection_set)
+
+# difference_set = set1 - set2
+# print(difference_set)
+
+# symmetric = set1 ^ set2
+# print(symmetric)
+
+# frozen_fruits = frozenset (["apple", "orange", "grape"])
+# print(frozen_fruits)
+
+if x > 10
+print("x is greater than 5")
